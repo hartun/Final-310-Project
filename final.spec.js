41be8f2f5048961f4852d931bc516f8baa42b0d5
Harry Tunnell <hartun@inref-llc.com>
2024-12-01 18:02:43 -0500

    unit test setup

---
+++ final.spec.js
@@ -0,0 +1,12 @@
+//example test case
+//  describe('Dealer Should Draw no Ace test', () => {
+//     it('should determine if the dealer should take another card', () => {
+//         const dealerHand = [
+//             { displayVal: 'Ten', suit: 'hearts', val: 10 },
+//             { displayVal: 'Nine', suit: 'hearts', val: 9 },
+//         ];
+
+//         const isSoft = calcPoints(dealerHand).isSoft;
+//         expect(isSoft).toBeFalsy();
+//         });
+// });
