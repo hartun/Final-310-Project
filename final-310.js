d69673eded114417881319e4f4cf77e70cbb3651
Harry Tunnell <hartun@inref-llc.com>
2024-12-01 21:06:14 -0500

    baseline

--- final-310.js
+++ final-310.js
@@ -1,1 +1,7 @@
-console.log(ready)
+//console.log('ready')
+const options = {method: 'GET', headers: {accept: 'application/json'}};
+
+fetch('https://api.tomorrow.io/v4/weather/forecast?location=46805&apikey=DLP9YEUDu06SiE2fvph4HQe7R9ZKdAKp', options)
+  .then(response => response.json())
+  .then(response => console.log(response))
+  .catch(err => console.error(err));
