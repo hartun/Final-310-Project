7a9f3130e27781e566ba673ba5ff9e7338a42707
Harry Tunnell <hartun@inref-llc.com>
2024-12-04 06:57:34 -0500

    API Key update

--- final-310.js
+++ final-310.js
@@ -1,16 +1,43 @@
 //console.log('ready')
-const options = {method: 'GET', headers: {accept: 'application/json'}};
+const formEl = document.getElementById('');
+const yearEl = document.getElementById('year');
+const monthEl = document.getElementById('month');
+const dateEl = document.getElementById('date');
+const zipEl = document.getElementById('zip');
 
-fetch('https://api.tomorrow.io/v4/weather/forecast?location=46805&apikey=DLP9YEUDu06SiE2fvph4HQe7R9ZKdAKp', options)
-  .then(response => response.json())
-  .then(response => console.log(response))
-  .catch(err => console.error(err));
+formEl.addEventListener('submit', function(e) {
+  e.preventDefault();
 
-function helpMessage() {
-  alert('Text 206-867-5309 for help.');
-}
-setTimeout(helpMessage, 3000);
+  const year = yearEl.value;
+  const month = monthEl.value;
+  const date = dateEl.value;
 
-function stopMessage() {
-  clearTimeout(helpMessage);
-}
+  const URL = `https://api.tomorrow.io/v4/weather/forecast?location=${zip}&apikey=${API_KEY}`
+
+  fetch(URL).then((result) => {
+    return result.json()
+  }).then((data) => {
+    console.log(data);
+  })
+
+});
+
+
+
+
+
+// const options = {method: 'GET', headers: {accept: 'application/json'}};
+
+// fetch('https://api.tomorrow.io/v4/weather/forecast?location=46805&apikey=DLP9YEUDu06SiE2fvph4HQe7R9ZKdAKp', options)
+//   .then(response => response.json())
+//   .then(response => console.log(response))
+//   .catch(err => console.error(err));
+
+// function helpMessage() {
+//   alert('Text 206-867-5309 for help.');
+// }
+// setTimeout(helpMessage, 3000);
+
+// function stopMessage() {
+//   clearTimeout(helpMessage);
+// }
