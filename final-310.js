c9beae876339e1436d7ca806d8c94cb9ef993513
Harry Tunnell <hartun@inref-llc.com>
2024-12-08 13:51:30 -0500

    final

--- final-310.js
+++ final-310.js
@@ -1,26 +1,28 @@
 //using fetch to get the YouTube API
 const videoSection = document.querySelector('section');
+const loader = document.querySelector('.loader-box');
 
-fetch('https://youtube.googleapis.com/youtube/v3/playlistItems?part=snippet&playlistId=UU1M6N5LUj3c1JhiZXiNDq3A&key=AIzaSyDu0XoK-4KitG1sLxKzankX9DGd6Hx6PD4')
+function getVideo() {
+
+fetch('https://youtube.googleapis.com/youtube/v3/playlistItems?part=snippet&maxResults=1&playlistId=UUWgIYO_NDj9RyuqUXY4GttQ&key=AIzaSyDu0XoK-4KitG1sLxKzankX9DGd6Hx6PD4')
 .then(res => res.json())
 .then(data => {
-  data.items.forEach(el => {
-  videoSection.innerHTML =
+ loader.style.display = 'none';
+ data.items.forEach(el => {
+  videoSection.innerHTML = 
   `<a target="_blank" href="https://www.youtube.com/watch?v=${el.snippet.resourceId.videoId}" class="yt-video">
-      <img src="${el.snippet.thumbnails.maxres.url}" />
-      <h4>${el.snippet.title}</h4>
+  <img src="${el.snippet.thumbnails.medium.url}" />
+  <h4>${el.snippet.title}</h4>
   </a>`
   });
 });
+}
+getVideo();
 
 function helpMessage() {
   alert('Have questions about YouTube Channel Insights? Text (206) 867-5309.');
 }
 setTimeout(helpMessage, 2000);
-
-function stopMessage() {
-  clearTimeout(helpMessage);
-}
 
 //form validation
 const fName = document.getElementById('fName');
